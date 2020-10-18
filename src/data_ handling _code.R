6d167edd896d082515f3f879e1cb63f6d7622556
KatBex <katherinebexley@gmail.com>
2020-10-18 19:01:29 +0100

    Introducing code to handle cats data in correct format

---
+++ src/data_ handling _code.R
@@ -0,0 +1,11 @@
+library("tidyverse")
+cats_data <- read_csv(file = "data/feline-data.csv", col_types = cols(
+  coat = col_character(),
+  weight = col_double(),
+  likes_string = col_logical()))
+cats_data2 <- read_csv("data/feline-data_v2.csv", col_types = cols(
+  coat = col_character(),
+  weight = col_double(),
+  likes_string = col_logical()
+) )
+
