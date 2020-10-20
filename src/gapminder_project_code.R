db6d4896caaea174d748d3677420669ae6296df6
KatBex <katherinebexley@gmail.com>
2020-10-20 16:50:57 +0100

    Using tidyverse to modify and present the gapminder data set, including adding variables and summaries

--- src/gapminder_project_code.R
+++ src/gapminder_project_code.R
@@ -11,3 +11,19 @@ gapminderPopCharacter <- read_csv("./data/gapminder-FiveYearData.csv", col_types
 year_country_gdp <- select(gapminder,year,country,gdpPercap)
 gapminderEurope <- filter(gapminder, continent == "Europe")
 gapminderOld <- filter(gapminder, lifeExp > 60)
+Africa_selected_data <- gapminder %>% filter(continent == "Africa") %>% 
+  select(lifeExp,country,year) %>% 
+  print(Africa_selected_data)
+gapminder_totalgdp <- gapminder %>% 
+  mutate(gdp = gdpPercap * pop)
+europe_ranked_life_expectancy <- gapminder %>%
+  filter(continent == "Europe", year == 2007) %>%
+  select(country, lifeExp) %>%
+  mutate(Rank = min_rank(desc(lifeExp))) %>% 
+  arrange(Rank)
+mean_2007 <- gapminder %>%
+  filter (year == 2007) %>%
+  summarise(lifeExp_mean = mean(lifeExp))
+continent_mean_lifeExp <- gapminder %>% 
+  group_by(continent,year) %>% 
+  summarise(lifeExp_mean = mean(lifeExp))
