8e9a268c7a6cd7cb30485ae85f93474fd83a4391
KatBex <katherinebexley@gmail.com>
2020-10-22 18:40:39 +0100

    ggiraph code included

--- src/gapminder_project_code.R
+++ src/gapminder_project_code.R
@@ -27,3 +27,26 @@ mean_2007 <- gapminder %>%
 continent_mean_lifeExp <- gapminder %>% 
   group_by(continent,year) %>% 
   summarise(lifeExp_mean = mean(lifeExp))
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
+gapminder %>% group_by(year) %>% 
+  summarise(meanlifeExp = mean(lifeExp)) %>% 
+  ggplot(aes(x=year, y = meanlifeExp)) + geom_point()
+gapminder %>% 
+  ggplot(aes(x=year, y = lifeExp, group = country, color = continent)) + geom_line() + geom_point()
+gapminder %>% 
+  ggplot(aes(x=year, y = lifeExp,group = country)) + geom_line(aes(colour = continent)) + geom_point(aes(colour="yellow"))
+gapminder %>% 
+  ggplot(aes(x = year, y = lifeExp, group = country)) + geom_line() + facet_wrap("continent")
+#challenge 3 - present gdp per capita data over time
+gapminder %>% 
+  ggplot(aes(x = year, y = gdpPercap, group = country)) + geom_line(aes(colour = continent))
+gapminder %>% 
+  ggplot(aes(x=year, y = gdpPercap, group = country)) + geom_line() + facet_wrap("continent", scale = "free_y")
+#
+#challenge 4 - working with ggiraph
+library(ggplot2)
+library(ggiraph)
+country <- gapminder$country
+interactive_graph <- ggplot(data = gapminder, aes(x= year, y = gdpPercap, group = country)) + geom_line(aes(colour = continent)) + geom_point_interactive(aes(x=year, y = gdpPercap, tooltip = country)) 
+girafe(ggobj = interactive_graph)
